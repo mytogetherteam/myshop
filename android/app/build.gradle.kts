89de14c36f752f9026ab69db0a3b0fffa7d5f75e
mytogetherteam <63414053+Arkar-Zin-Naing@users.noreply.github.com>
2026-06-08 17:47:59 +0700

    update production

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.mytogether.myshop"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
